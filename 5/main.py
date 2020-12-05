de87fc593020db13e7cc83e25b3b5f9366d5a81d
Rafa leyva <rafaelleru95103@gmail.com>
2020-12-05 15:37:28 +0100

    Add solution for day 5 part 1

---
+++ 5/main.py
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+INPUT_FILE = './input/input.txt'
+
+class BoardPass(object):
+    def __init__(self, seat_spec):
+        # TODO: Esto seguro que se puede simplificar
+        start, end = 0, 127
+
+        for mov in seat_spec[:-3]:
+            pivot = start + (end-start)//2
+            if mov == 'B':
+                start = pivot + 1
+            else:
+                end = pivot
+
+        self.row = start
+
+        start, end = 0, 7
+        for mov in seat_spec[-3:]:
+            pivot = start + (end-start)//2
+            if mov == 'L':
+                end = pivot
+            else:
+                start = pivot + 1
+
+        self.colum = start
+
+    @property
+    def id(self):
+        return self.row*8 + self.colum
+
+
+if __name__ == '__main__':
+    f = open(INPUT_FILE)
+    passes = [BoardPass(x.rstrip()) for x in f.readlines()]
+    sol = max([p.id for p in passes])
+    print("Higest pass id is: {}".format(sol))
