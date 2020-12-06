c7c2b928af11490154286dd7ba6187d1252d8710
Rafa leyva <rafaelleru95103@gmail.com>
2020-12-06 11:13:04 +0100

    Remove class GroupAnswers, it was difficulting solution to part2

--- 6/main.py
+++ 6/main.py
@@ -2,23 +2,14 @@
 
 INPUT_FILE = './input/input.txt'
 
-class GroupAnswers():
-    def __init__(self, raw_data):
-        self.unique_answers = {x for x in raw_data}
-
-    @property
-    def yes_count(self):
-        return len(self.unique_answers)
-
-
-def solve_1():
-    with open(INPUT_FILE) as f:
-        raw_data = f.read()
-        groups = [GroupAnswers(x.rstrip().replace('\n', '')) for x in raw_data.split('\n\n')]
-
-    return sum([g.yes_count for g in groups])
+def solve_1(raw_data):
+    groups = [{x for x in raw_ansers.rstrip().replace('\n', '')} for raw_ansers in raw_data.split('\n\n')]
+    return sum([len(g) for g in groups])
 
 
 if __name__ == '__main__':
-    sol_1 = solve_1()
+    with open(INPUT_FILE) as f:
+        raw_data = f.read()
+
+    sol_1 = solve_1(raw_data)
     print("Total sum of yes answers is: {}".format(sol_1))
