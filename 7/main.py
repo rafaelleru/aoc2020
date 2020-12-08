ef2a9e5db203f11ef1e5f5a106f86a247bde721a
Rafa leyva <rafaelleru95103@gmail.com>
2020-12-08 12:18:56 +0100

    Add solution for day 7 part 2

--- 7/main.py
+++ 7/main.py
@@ -4,10 +4,41 @@ INPUT_FILE = './input/input.txt'
 COLOR_REGEX = '(\w+ \w+) bags contain'
 
 class Node(object):
-    def __init__(self, color, contains={}, is_contained_in={}):
+    def __init__(self, color, contains={}):
         self.color = color
         self.contains = contains
-        self.is_contained_in = is_contained_in
+        self.cardinalities = {}
+
+
+def build_rules_graph(rules):
+    index = {}
+    # Contruimos un grafo con la informacion de las mochilas
+    for r in rules:
+        if 'no other bags' in r:
+            node = Node(re.match(COLOR_REGEX, r).group(1))
+            index[node.color] = node
+        else:
+            childrens = {}
+            cardinalities = {}
+            for match in re.findall("(?P<cardinality>(\d) (\w+ \w+) bags?)", r.split('contain')[1]):
+                cardinalities[match[2]] = int(match[1])
+                if match[2] in index:
+                    childrens[match[2]] = index[match[2]]
+                else:
+                    node = Node(match[2], {})
+                    childrens[match[2]] = node
+                    index[match[2]] = node
+
+            color = re.match(COLOR_REGEX, r).group(1)
+            if color in index:
+                index[color].contains.update(childrens)
+                index[color].cardinalities.update(cardinalities)
+            else:
+                index[color] = Node(color, childrens)
+                index[color].cardinalities.update(cardinalities)
+
+    return index
+
 
 def can_contain_color(node, color):
     if color in node.contains:
@@ -20,34 +51,18 @@ def can_contain_color(node, color):
 
         return {}
 
-def build_rules_graph(rules):
-    nodes_index = {}
-    # Contruimos un grafo con la informacion de las mochilas
-    for r in rules:
-        if 'no other bags' in r:
-            node = Node(re.match(COLOR_REGEX, r).group(1))
-            nodes_index[node.color] = node
-        else:
-            childrens = {}
-            for match in re.findall("(\d (\w+ \w+) bags?)", r.split('contain')[1]):
-                if match[1] in nodes_index:
-                    childrens[match[1]] = nodes_index[match[1]]
-                else:
-                    node = Node(match[1], {})
-                    childrens[match[1]] = node
-                    nodes_index[match[1]] = node
+def count_bags_inside(node):
+    if node.contains == {}:
+        return 0
+    else:
+        inside = 0
+        for color, cardinality in node.cardinalities.items():
+            inside += cardinality + cardinality*count_bags_inside(node.contains[color])
 
-            color = re.match(COLOR_REGEX, r).group(1)
-            if color in nodes_index:
-                nodes_index[color].contains.update(childrens)
-            else:
-                nodes_index[color] = Node(color, childrens)
+        return inside
 
-    return nodes_index
 
-def solve_1(rules):
-    nodes_index = build_rules_graph(rules)
-
+def solve_1(nodes_index):
     valids = set()
     for bag in nodes_index.values():
         valids.update(can_contain_color(bag, 'shiny gold'))
@@ -55,9 +70,17 @@ def solve_1(rules):
     return len(valids)
 
 
+def solve_2(nodes_index):
+    return count_bags_inside(nodes_index['shiny gold'])
+
+
 if __name__ == '__main__':
     with open(INPUT_FILE) as input:
         rules = input.readlines()
 
-    bags = solve_1(rules)
+    nodes_index = build_rules_graph(rules)
+    bags = solve_1(nodes_index)
     print("Number of bags that can contain a shiny gold bag is: ", bags)
+
+    bags = solve_2(nodes_index)
+    print("Number of bags that shiny gold must contain is: ", bags)
