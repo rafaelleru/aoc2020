7342de305e7af97db85b5d39e1921f08e481e127
rafaelleru <rafaelleru95103@gmail.com>
2020-12-15 23:30:58 +0100

    WIP: Day 13 part 2 not working

--- 13/main.py
+++ 13/main.py
@@ -1,6 +1,6 @@
 import math
 
-INPUT_FILE = './input/input.txt'
+INPUT_FILE = './input/example.txt'
 
 def parse_input():
     with open(INPUT_FILE) as f:
@@ -11,6 +11,8 @@ def parse_input():
         for b in line:
             if b != 'x':
                 buses.append(int(b))
+            else:
+                buses.append(b)
 
     return ts, buses
 
@@ -19,19 +21,39 @@ def solve_1(ts, bus_schedule):
     bus_id = bus_schedule[0]
 
     for b in bus_schedule:
+        if b == 'x':
+            continue
         if b - int(ts%b) < wait:
             bus_id = b
             wait = b - int(ts%b)
 
     print(bus_id*wait)
 
+
 def solve_2(bus_schedule):
-    pass
+    timestamp = 0
+    s_i = 0
+    n = 1
+
+    partial_sols = []
+
+    for bus_id in bus_schedule:
+        if type(bus_id) == int:
+            n *= bus_id
+
+    for i, bus_id in enumerate(bus_schedule):
+        if bus_id == 'x':
+            continue
+
+        print("X === {} ({})".format(((bus_id-i)%bus_id), bus_id))
+        s_i = pow(n//bus_id, -1, bus_id)
+        partial_sols.append(((bus_id-i)%bus_id)*bus_id*s_i)
+
+    print(sum(partial_sols))
 
 
 
 if __name__=='__main__':
     ts, bus_schedule = parse_input()
     solve_1(ts, bus_schedule)
-
     solve_2(bus_schedule)
