092c98901a4af75442799e398dd1330cba2caf05
rafaelleru <rafaelleru95103@gmail.com>
2020-12-16 19:11:00 +0100

    Add solution for day 15 part 1

---
+++ 15/main.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+def solve_1(numbers, nturns=2020):
+    history = defaultdict(list)
+    for i, n in enumerate(numbers):
+        history[n] = [i+1]
+
+    last = 0
+    was_new = True
+
+    for i in range(len(numbers)+1, nturns+1):
+        if was_new:
+            last = 0  # se dice el numero 0
+        else:
+            last = history[last][-1] - history[last][-2]
+
+        was_new = last not in history
+        history[last].append(i)
+
+    print(last)
+
+if __name__ == '__main__':
+    # numbers = [0,3,6] # Example
+    numbers = [19,20,14,0,9,1] # Input
+    solve_1(numbers, 2020)
